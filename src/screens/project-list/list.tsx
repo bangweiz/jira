7cd486376059e6f269daf3503edac884fc19d9fb
bangweizhou <bangwei.zhou@dealersolutions.com.au>
2022-04-25 14:12:48 +1000

    change js to ts

---
+++ src/screens/project-list/list.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { User } from "./search-panel";
+
+interface Project {
+  id: string;
+  name: string;
+  personId: string;
+  pin: boolean;
+  organization: string;
+}
+
+interface ListProps {
+  list: Project[];
+  users: User[];
+}
+
+export const List = ({ list, users }: ListProps) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Project</th>
+          <th>Owner</th>
+        </tr>
+      </thead>
+      <tbody>
+        {list.map((project) => (
+          <tr key={project.id}>
+            <td>{project.name}</td>
+            <td>
+              {users.find((user) => user.id === project.personId)?.name ||
+                "unknown"}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,23 +1,25 @@
 import { useEffect, useState } from "react";
 
-export const isFalse = (value) => (value === 0 ? false : !value);
+export const isFalse = (value: unknown) => (value === 0 ? false : !value);
 
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
     const value = result[key];
     if (isFalse(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(callback, []);
 };
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
