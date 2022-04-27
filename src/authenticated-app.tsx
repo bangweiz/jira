ed30c0459bcab3669475fb6db3445a4d12c8a1b4
bangweizhou <bangwei.zhou@dealersolutions.com.au>
2022-04-28 09:59:55 +1000

    feature: add router

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Navigate, Route, Routes } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import { ProjectListScreen } from "./screens/project-list";
 import { useAuth } from "./context/auth-context";
@@ -6,39 +8,52 @@ import styled from "@emotion/styled";
 import { Row } from "./components/lib";
 import { ReactComponent as SoftwareLogo } from "assets/software-logo.svg";
 import { Button, Dropdown, Menu } from "antd";
+import { ProjectScreen } from "./screens/project";
 
 export const AuthenticatedApp = () => {
-  const { logout, user } = useAuth();
   return (
     <Container>
-      <Header between={true}>
-        <HeaderLeft gap={true}>
-          <SoftwareLogo width="18rem" color="rbg(38, 132, 255)" />
-          <h3>Project</h3>
-          <h3>User</h3>
-        </HeaderLeft>
-        <HeaderRight>
-          <Dropdown
-            overlay={
-              <Menu>
-                <Menu.Item key="logout">
-                  <Button type="link" onClick={logout}>
-                    Log out
-                  </Button>
-                </Menu.Item>
-              </Menu>
-            }
-          >
-            <Button type="link" onClick={(e) => e.preventDefault()}>
-              Hi, {user?.name}
-            </Button>
-          </Dropdown>
-        </HeaderRight>
-      </Header>
+      <PageHeader />
       <Main>
-        <ProjectListScreen />
+        <Router>
+          <Routes>
+            <Route path="/project" element={<ProjectListScreen />} />
+            <Route path="/project/:projectId/*" element={<ProjectScreen />} />
+          </Routes>
+        </Router>
       </Main>
     </Container>
+  );
+};
+
+const PageHeader = () => {
+  const { logout, user } = useAuth();
+
+  return (
+    <Header between={true}>
+      <HeaderLeft gap={true}>
+        <SoftwareLogo width="18rem" color="rbg(38, 132, 255)" />
+        <h3>Project</h3>
+        <h3>User</h3>
+      </HeaderLeft>
+      <HeaderRight>
+        <Dropdown
+          overlay={
+            <Menu>
+              <Menu.Item key="logout">
+                <Button type="link" onClick={logout}>
+                  Log out
+                </Button>
+              </Menu.Item>
+            </Menu>
+          }
+        >
+          <Button type="link" onClick={(e) => e.preventDefault()}>
+            Hi, {user?.name}
+          </Button>
+        </Dropdown>
+      </HeaderRight>
+    </Header>
   );
 };
 
