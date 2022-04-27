6dbc74f6a7bd011e5d7222bf03d72319a8085789
bangweizhou <bangwei.zhou@dealersolutions.com.au>
2022-04-27 16:21:22 +1000

    dealer with loading and error

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,4 +1,6 @@
+import React from "react";
 import styled from "@emotion/styled";
+import { Spin, Typography } from "antd";
 
 export const Row = styled.div<RowProps>`
   display: flex;
@@ -23,3 +25,26 @@ interface RowProps {
   between?: boolean;
   marginBottom?: number;
 }
+
+const FullPage = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FullPageLoading = () => {
+  return (
+    <FullPage>
+      <Spin size="large" />
+    </FullPage>
+  );
+};
+
+export const FullPageEorrorFallback = ({ error }: { error: Error | null }) => {
+  return (
+    <FullPage>
+      <Typography.Text type="danger">{error?.message}</Typography.Text>
+    </FullPage>
+  );
+};
