aa9d38e265263411cd8891f02b1b87f6f69e912c
bangweizhou <bangwei.zhou@dealersolutions.com.au>
2022-04-27 13:01:38 +1000

    style: add flex and grid

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,13 +2,40 @@ import React from "react";
 
 import { ProjectListScreen } from "./screens/project-list";
 import { useAuth } from "./context/auth-context";
+import styled from "@emotion/styled";
+import { Row } from "./components/lib";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>Log out</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h3>Logo</h3>
+          <h3>Project</h3>
+          <h3>User</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>Log out</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  height: 100vh;
+`;
+
+const Main = styled.main``;
+
+const Header = styled(Row)``;
+
+const HeaderLeft = styled(Row)``;
+
+const HeaderRight = styled.div``;
