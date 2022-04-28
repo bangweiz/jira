9f94a8e16b8fdea61eab353e62942d2cb810a5e0
bangweizhou <bangwei.zhou@dealersolutions.com.au>
2022-04-28 12:32:18 +1000

    feature: add id-select

---
+++ src/components/id-select.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Select } from "antd";
+import { Raw } from "../types";
+
+type SelectProps = React.ComponentProps<typeof Select>;
+
+interface IdSelectProps
+  extends Omit<
+    SelectProps,
+    "value" | "onChange" | "defaultOptionName" | "options"
+  > {
+  value: Raw | null | undefined;
+  onChange: (value?: number) => void;
+  defaultOptionName?: string;
+  options?: { name: string; id: number }[];
+}
+
+export const IdSelect = (props: IdSelectProps) => {
+  const { value, onChange, defaultOptionName, options, ...resProps } = props;
+
+  return (
+    <Select
+      value={toNumber(value)}
+      onChange={(value) => onChange(toNumber(value) || undefined)}
+      {...resProps}
+    >
+      {defaultOptionName ? (
+        <Select.Option value={0}>{defaultOptionName}</Select.Option>
+      ) : null}
+      {options?.map((option) => (
+        <Select.Option key={option.name} value={option.id}>
+          {option.name}
+        </Select.Option>
+      ))}
+    </Select>
+  );
+};
+
+const toNumber = (value: unknown) => (isNaN(Number(value)) ? 0 : Number(value));
