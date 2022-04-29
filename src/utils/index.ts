645e4392c5485274d8b72388f571f393391601f7
bangweizhou <bangwei.zhou@dealersolutions.com.au>
2022-04-30 09:12:56 +1000

    fixed the error that sends data when page unmounted

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -56,3 +56,15 @@ export const useDocumentTitle = (
 };
 
 export const resetRoute = () => (window.location.href = window.location.origin);
+
+export const useMountedRef = () => {
+  const mountedRef = useRef(false);
+  useEffect(() => {
+    mountedRef.current = true;
+    return () => {
+      mountedRef.current = false;
+    };
+  });
+
+  return mountedRef;
+};
--- src/utils/use-async.ts
+++ src/utils/use-async.ts
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useMountedRef } from "./index";
 
 interface State<D> {
   error: Error | null;
@@ -25,6 +26,7 @@ export const useAsync = <D>(
     ...defaultInitState,
     ...initState,
   });
+  const mountedRef = useMountedRef();
   const [retry, setRetry] = useState(() => () => {});
 
   const setData = (data: D) =>
@@ -56,7 +58,9 @@ export const useAsync = <D>(
     setState({ ...state, stat: "loading" });
     return promise
       .then((data) => {
-        setData(data);
+        if (mountedRef.current) {
+          setData(data);
+        }
         return data;
       })
       .catch((error) => {
